cbc0541c0c9caa2eefccf2aa7e3910143ac3faf8
zdh1207git <gotoseeyou@qq.com>
2019-12-24 14:40:19 +0800

    :sparkle:register

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,12 +1,32 @@
 <template>
-  <div>
-    <h1>Register</h1>
-    <input type="email" name="email" v-model="email" placeholder="email" />
-    <br />
-    <input type="password" name="password" v-model="password" placeholder="password" />
-    <br />
-    <button @click="register">Register</button>
-  </div>
+  <v-layout row>
+    <v-flex xs6 offset-xs3>
+      <div class="white elevation-2">
+        <v-toolbar flat dense class="cyan" dark>
+          <v-toolbar-title>Register</v-toolbar-title>
+        </v-toolbar>
+        <div class="pl-4 pr-2 pt-2 pb-2">
+          <input
+            type="email"
+            name="email"
+            v-model="email"
+            placeholder="email"
+          />
+          <br />
+          <input
+            type="password"
+            name="password"
+            v-model="password"
+            placeholder="password"
+          />
+          <br />
+          <div class="error" v-html="error">{{ error }}</div>
+          <br />
+          <v-btn class="cyan" @click="register">Register</v-btn>
+        </div>
+      </div>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
@@ -16,19 +36,27 @@ export default {
   data() {
     return {
       email: "abc",
-      password: "123"
+      password: "123",
+      error: null
     };
   },
   methods: {
     async register() {
-      await AuthenticationService.register({
-        email: this.email,
-        password: this.password
-      });
+      try {
+        await AuthenticationService.register({
+          email: this.email,
+          password: this.password
+        });
+      } catch (error) {
+        this.error = error.response.data.error;
+      }
     }
   }
 };
 </script>
 
 <style scoped>
+.error {
+  color: red;
+}
 </style>
--- server/src/app.js
+++ server/src/app.js
@@ -3,16 +3,19 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
-
+const config = require('./config/config')
+const {
+  sequelize
+} = require('./models')
 const app = express()
 
 app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(cors());
 
-app.post('/register', (req, res) => {
-  res.send({
-    message: `Hello, ${req.body.email}! Your user was registered! Have fun!`
-  })
+require('./routes')(app)
+
+sequelize.sync().then(() => {
+  app.listen(config.port);
+  console.log(`Server started on port ${config.port}`)
 })
-app.listen(process.env.PORT || 8081);
